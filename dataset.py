1689fd70f5af0daff57d9c92938c2297c0cc13d6
Darcey Riley <Darcey.Riley@nd.edu>
2023-04-07 16:05:42 -0400

    Add code for batching training datasets

---
+++ dataset.py
@@ -0,0 +1,99 @@
+# TODO(darcey): write the seq2seq test dataset code
+# TODO(darcey): write dataset classes for classification tasks
+
+import random
+import numpy as np
+import torch
+import vocabulary
+
+
+
+class Seq2SeqTrainDataset():
+
+    # assumes src, tgt are lists of lists of token indices
+    # sorts by tgt length because that determines number of training steps
+    def __init__(self, src, tgt, batch_size, vocab_size):
+        sorted_src, sorted_tgt = self.sort_by_tgt_len(src, tgt)
+        self.batches = self.make_batches(sorted_src, sorted_tgt, batch_size, vocab_size)
+        self.num_iters = 0
+        self.batch_iter = self.get_batch_iter()
+
+    def sort_by_tgt_len(self, src, tgt):
+        lens = [len(tgt_sent) for tgt_sent in tgt]
+        sorted_idxs = np.argsort(lens)
+        sorted_src = [src[i] for i in sorted_idxs]
+        sorted_tgt = [tgt[i] for i in sorted_idxs]
+        return sorted_src, sorted_tgt
+
+    def make_batches(self, src, tgt, batch_size, vocab_size):
+        batches = []
+
+        curr_batch = []
+        curr_num_toks = 0
+        for (src_sent, tgt_sent) in zip(src, tgt):
+            curr_batch.append((src_sent, tgt_sent))
+            curr_num_toks += len(tgt_sent)
+            if curr_num_toks >= batch_size:
+                batches.append(self.make_one_batch(curr_batch, vocab_size))
+                curr_batch = []
+                curr_num_toks = 0
+        if len(curr_batch) > 0:
+            batches.append(self.make_one_batch(curr_batch, vocab_size))
+
+        return batches
+
+    def make_one_batch(self, sent_list, vocab_size):
+        num_sents   = len(sent_list)
+        max_src_len = max([len(src_sent) for (src_sent, _) in sent_list])
+        max_tgt_len = max([len(tgt_sent) for (_, tgt_sent) in sent_list])
+
+        src_tensor     = torch.full((num_sents, max_src_len+1), 0)
+        tgt_in_tensor  = torch.full((num_sents, max_tgt_len+1), 0)
+        tgt_out_tensor = torch.full((num_sents, max_tgt_len+1), 0)
+
+        PAD = [vocabulary.SpecialTokens.PAD.value]
+        BOS = [vocabulary.SpecialTokens.BOS.value]
+        EOS = [vocabulary.SpecialTokens.EOS.value]
+        for i, (src_sent, tgt_sent) in enumerate(sent_list):
+            src_tensor[i]     = torch.tensor(src_sent + EOS + PAD*(max_src_len - len(src_sent)))
+            tgt_in_tensor[i]  = torch.tensor(BOS + tgt_sent + PAD*(max_tgt_len - len(tgt_sent)))
+            tgt_out_tensor[i] = torch.tensor(tgt_sent + EOS + PAD*(max_tgt_len - len(tgt_sent)))
+
+        num_src_toks = torch.sum(src_tensor != vocabulary.SpecialTokens.PAD.value)
+        num_tgt_toks = torch.sum(tgt_in_tensor != vocabulary.SpecialTokens.PAD.value)
+
+        return {
+            "src": torch.nn.functional.one_hot(src_tensor, vocab_size),
+            "tgt_in": torch.nn.functional.one_hot(tgt_in_tensor, vocab_size),
+            "tgt_out": torch.nn.functional.one_hot(tgt_out_tensor, vocab_size),
+            "num_src_toks": num_src_toks,
+            "num_tgt_toks": num_tgt_toks,
+        }
+
+    def get_batch(self):
+        try:
+            batch = next(self.batch_iter)
+        except StopIteration:
+            self.num_iters += 1
+            self.batch_iter = self.get_batch_iter()
+            batch = next(self.batch_iter)
+        return batch
+
+    def get_batch_iter(self):
+        order = list(range(len(self.batches)))
+        random.shuffle(order)
+        for i in order:
+            yield self.batches[i]
+
+
+
+#class Seq2SeqTranslateDataset():
+#    def __init__(self, src):
+        # sort src by length
+        # need to keep track of original idxs for organizing the translations
+        # (maybe include an option of whether it should be length batched or not? might want to do translation in dataset order so we can print intermediate results / clear the buffer, for cases with large # of samples)
+        # divide up into batches by number of concurrent beams
+        # need to consider very large (larger than one batch) numbers of samples; that should be dealt with during batching
+        # need iterator that gives the next batch
+        # need function that takes in translations and adds them to the ordered list of translations
+        # need to think about how this will interact with writing the translations to file
---
+++ test_dataset.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn.functional as F
+import unittest
+from dataset import *
+
+
+
+class TestSeq2SeqTrainDataset(unittest.TestCase):
+
+    def testSortByTgtLen(self):
+        src = [[1,2,3,4],[1,2,3,4,5],[1,2],[1,2,3]]
+        tgt = [[10,11,12,13,14],[10,11,12,13],[10,11,12],[10,11,12,13,14,15]]
+        ds = Seq2SeqTrainDataset(src, tgt, 100, 16)
+        
+        sorted_src_correct = [[1,2],[1,2,3,4,5],[1,2,3,4],[1,2,3]]
+        sorted_tgt_correct = [[10,11,12],[10,11,12,13],[10,11,12,13,14],[10,11,12,13,14,15]]
+        sorted_src_actual, sorted_tgt_actual = ds.sort_by_tgt_len(src, tgt)
+        self.assertEqual(sorted_src_actual, sorted_src_correct)
+        self.assertEqual(sorted_tgt_actual, sorted_tgt_correct)
+
+    def testMakeOneBatch(self):
+        src_sents = [[5,6,7,8],[5,6,7,8,9],[5,6],[5,6,7]]
+        tgt_sents = [[10,11,12,13,14],[10,11,12,13],[10,11,12],[10,11,12,13,14,15]]
+        sent_list = list(zip(src_sents, tgt_sents))
+        ds = Seq2SeqTrainDataset(src_sents, tgt_sents, 100, 16)
+        
+        PAD = vocabulary.SpecialTokens.PAD.value
+        BOS = vocabulary.SpecialTokens.BOS.value
+        EOS = vocabulary.SpecialTokens.EOS.value
+        src_correct = torch.tensor([[5,6,7,8,EOS,PAD],
+                                    [5,6,7,8,9,EOS],
+                                    [5,6,EOS,PAD,PAD,PAD],
+                                    [5,6,7,EOS,PAD,PAD]])
+        tgt_in_correct = torch.tensor([[BOS,10,11,12,13,14,PAD],
+                                       [BOS,10,11,12,13,PAD,PAD],
+                                       [BOS,10,11,12,PAD,PAD,PAD],
+                                       [BOS,10,11,12,13,14,15]])
+        tgt_out_correct = torch.tensor([[10,11,12,13,14,EOS,PAD],
+                                        [10,11,12,13,EOS,PAD,PAD],
+                                        [10,11,12,EOS,PAD,PAD,PAD],
+                                        [10,11,12,13,14,15,EOS]])
+        actual = ds.make_one_batch(sent_list, 16)
+        self.assertTrue(torch.equal(actual["src"], F.one_hot(src_correct,16)))
+        self.assertTrue(torch.equal(actual["tgt_in"], F.one_hot(tgt_in_correct,16)))
+        self.assertTrue(torch.equal(actual["tgt_out"], F.one_hot(tgt_out_correct,16)))
+        self.assertEqual(actual["num_src_toks"], 18)
+        self.assertEqual(actual["num_tgt_toks"], 22)
+
+    def testMakeBatches(self):
+        src_sents = [[5,6],[5,6,8,9],[5,6,7,8,9],[5,6,7,8],[5,6,7]]
+        tgt_sents = [[10,11,12],[10,11,12,13],[10,11,12,13],[10,11,12,13,14],[10,11,12,13,14,15]]
+        ds = Seq2SeqTrainDataset(src_sents, tgt_sents, 7, 16)
+
+        PAD = vocabulary.SpecialTokens.PAD.value
+        BOS = vocabulary.SpecialTokens.BOS.value
+        EOS = vocabulary.SpecialTokens.EOS.value
+        src_correct_1 = F.one_hot(torch.tensor([[5,6,EOS,PAD,PAD],[5,6,8,9,EOS]]),16)
+        src_correct_2 = F.one_hot(torch.tensor([[5,6,7,8,9,EOS],[5,6,7,8,EOS,PAD]]),16)
+        src_correct_3 = F.one_hot(torch.tensor([[5,6,7,EOS]]),16)
+        tgt_in_correct_1 = F.one_hot(torch.tensor([[BOS,10,11,12,PAD],[BOS,10,11,12,13]]),16)
+        tgt_in_correct_2 = F.one_hot(torch.tensor([[BOS,10,11,12,13,PAD],[BOS,10,11,12,13,14]]),16)
+        tgt_in_correct_3 = F.one_hot(torch.tensor([[BOS,10,11,12,13,14,15]]),16)
+        tgt_out_correct_1 = F.one_hot(torch.tensor([[10,11,12,EOS,PAD],[10,11,12,13,EOS]]),16)
+        tgt_out_correct_2 = F.one_hot(torch.tensor([[10,11,12,13,EOS,PAD],[10,11,12,13,14,EOS]]),16)
+        tgt_out_correct_3 = F.one_hot(torch.tensor([[10,11,12,13,14,15,EOS]]),16)
+
+        actual = ds.make_batches(src_sents, tgt_sents, 7, 16)
+
+        self.assertEqual(len(actual), 3)
+        self.assertTrue(torch.equal(actual[0]["src"], src_correct_1))
+        self.assertTrue(torch.equal(actual[0]["tgt_in"], tgt_in_correct_1))
+        self.assertTrue(torch.equal(actual[0]["tgt_out"], tgt_out_correct_1))
+        self.assertEqual(actual[0]["num_src_toks"], 8)
+        self.assertEqual(actual[0]["num_tgt_toks"], 9)
+        self.assertTrue(torch.equal(actual[1]["src"], src_correct_2))
+        self.assertTrue(torch.equal(actual[1]["tgt_in"], tgt_in_correct_2))
+        self.assertTrue(torch.equal(actual[1]["tgt_out"], tgt_out_correct_2))
+        self.assertEqual(actual[1]["num_src_toks"], 11)
+        self.assertEqual(actual[1]["num_tgt_toks"], 11)
+        self.assertTrue(torch.equal(actual[2]["src"], src_correct_3))
+        self.assertTrue(torch.equal(actual[2]["tgt_in"], tgt_in_correct_3))
+        self.assertTrue(torch.equal(actual[2]["tgt_out"], tgt_out_correct_3))
+        self.assertEqual(actual[2]["num_src_toks"], 4)
+        self.assertEqual(actual[2]["num_tgt_toks"], 7)
+
+    def testInit(self):
+        src_sents = [[5,6,7,8],[5,6,7,8,9],[5,6],[5,6,7],[5,6,8,9]]
+        tgt_sents = [[10,11,12,13,14],[10,11,12,13],[10,11,12],[10,11,12,13,14,15],[10,11,12,13]]
+        ds = Seq2SeqTrainDataset(src_sents, tgt_sents, 7, 16)
+        
+        PAD = vocabulary.SpecialTokens.PAD.value
+        BOS = vocabulary.SpecialTokens.BOS.value
+        EOS = vocabulary.SpecialTokens.EOS.value
+        src_correct_1 = F.one_hot(torch.tensor([[5,6,EOS,PAD,PAD,PAD],[5,6,7,8,9,EOS]]),16)
+        src_correct_2 = F.one_hot(torch.tensor([[5,6,8,9,EOS],[5,6,7,8,EOS]]),16)
+        src_correct_3 = F.one_hot(torch.tensor([[5,6,7,EOS]]),16)
+        tgt_in_correct_1 = F.one_hot(torch.tensor([[BOS,10,11,12,PAD],[BOS,10,11,12,13]]),16)
+        tgt_in_correct_2 = F.one_hot(torch.tensor([[BOS,10,11,12,13,PAD],[BOS,10,11,12,13,14]]),16)
+        tgt_in_correct_3 = F.one_hot(torch.tensor([[BOS,10,11,12,13,14,15]]),16)
+        tgt_out_correct_1 = F.one_hot(torch.tensor([[10,11,12,EOS,PAD],[10,11,12,13,EOS]]),16)
+        tgt_out_correct_2 = F.one_hot(torch.tensor([[10,11,12,13,EOS,PAD],[10,11,12,13,14,EOS]]),16)
+        tgt_out_correct_3 = F.one_hot(torch.tensor([[10,11,12,13,14,15,EOS]]),16)
+
+        actual = ds.batches
+
+        self.assertEqual(len(actual), 3)
+        self.assertTrue(torch.equal(actual[0]["src"], src_correct_1))
+        self.assertTrue(torch.equal(actual[0]["tgt_in"], tgt_in_correct_1))
+        self.assertTrue(torch.equal(actual[0]["tgt_out"], tgt_out_correct_1))
+        self.assertEqual(actual[0]["num_src_toks"], 9)
+        self.assertEqual(actual[0]["num_tgt_toks"], 9)
+        self.assertTrue(torch.equal(actual[1]["src"], src_correct_2))
+        self.assertTrue(torch.equal(actual[1]["tgt_in"], tgt_in_correct_2))
+        self.assertTrue(torch.equal(actual[1]["tgt_out"], tgt_out_correct_2))
+        self.assertEqual(actual[1]["num_src_toks"], 10)
+        self.assertEqual(actual[1]["num_tgt_toks"], 11)
+        self.assertTrue(torch.equal(actual[2]["src"], src_correct_3))
+        self.assertTrue(torch.equal(actual[2]["tgt_in"], tgt_in_correct_3))
+        self.assertTrue(torch.equal(actual[2]["tgt_out"], tgt_out_correct_3))
+        self.assertEqual(actual[2]["num_src_toks"], 4)
+        self.assertEqual(actual[2]["num_tgt_toks"], 7)
+
+    def testIter(self):
+        src_sents = [[5,6,7,8],[5,6,7,8,9],[5,6],[5,6,7],[5,6,8,9]]
+        tgt_sents = [[10,11,12,13,14],[10,11,12,13],[10,11,12],[10,11,12,13,14,15],[10,11,12,13]]
+        ds = Seq2SeqTrainDataset(src_sents, tgt_sents, 7, 16)
+        ds.batches = [1, 2, 3, 4]
+        
+        # test one iteration
+        correct_batches = set([1, 2, 3, 4])
+        actual_batches = set()
+        for i in range(4):
+            actual_batches.add(ds.get_batch())
+        self.assertTrue(actual_batches, correct_batches)
+        
+        # test multiple iteration at a time
+        correct_batches = set([1, 2, 3, 4])
+        actual_batches = set()
+        for i in range(20):
+            b = ds.get_batch()
+            if i > 16:
+                actual_batches.add(b)
+        self.assertTrue(actual_batches, correct_batches)
+        
+        # test iteration counting
+        self.assertEqual(ds.num_iters, 5)
